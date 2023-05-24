2263ccd0f9f1c11c2bb506bc29f283db6c8f83b9
krulci <52474054+krulci@users.noreply.github.com>
2023-05-24 03:31:51 -0400

    Add project files.

---
+++ FullScreenizer/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace FullScreenizer
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            AddComponent<Fullscreenizer>();
+        }
+    }
+}
